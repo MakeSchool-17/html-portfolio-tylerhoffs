69d4fed15a0fa26b7e427f64a83bf6fe6f30f979
tylerhoffs <tyler.hoffman@students.makeschool.com>
2016-01-19 10:06:20 -0800

    Starting Website

---
+++ js/portfolio.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    //alert('hello');
+    $('#gallery').justifiedGallery({
+       rowHeight : 300,
+       lastRow : 'justify',
+       margins : 3,
+       randomize: true
+     });
+});
